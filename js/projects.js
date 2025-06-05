ff1972b241ba82bb4e22dce62b29dc43ca12287a
terrainthesky-hub <lesley.t.rich@gmail.com>
2025-06-05 13:21:11 -0700

    project image changes

--- js/projects.js
+++ js/projects.js
@@ -3,7 +3,7 @@ const projects = [
         title: "CAD Designs",
         description: "3D modeling of complex objects like a Turner's Cube and a multi-part lantern using Fusion 360.",
         tags: ["CAD", "Fusion 360", "3D Modeling"],
-        image: "assets/images/turners_cube.png", // This is your turner's cube image
+        image: "assets/images/cnc_portfolio/Turners_Cube.png", // This is your turner's cube image
         liveUrl: "cad-designs.html", // Links to the new page
         repoUrl: "" 
     },
@@ -11,7 +11,7 @@ const projects = [
         title: "CNC & Manual Machining",
         description: "Projects created using a Genmitsu CNC router and a manual mill, including styrofoam shapes and a 6-sided die.",
         tags: ["CNC", "CAM", "Manual Mill", "Machining"],
-        image: "assets/images/Genmitsu_CNC.png", // Use the Genmitsu image as a thumbnail
+        image: "assets/images/cnc_portfolio/Genmitsu.png", // Use the Genmitsu image as a thumbnail
         liveUrl: "cnc-machining.html", // Links to the new page
         repoUrl: "" 
     },
@@ -19,7 +19,7 @@ const projects = [
         title: "Metal Lettering Experiments",
         description: "Creating custom letter shapes from metal stock using various tools like bandsaws, jigsaws, and dremels.",
         tags: ["Metalwork", "Bandsaw", "Fabrication"],
-        image: "assets/images/Second_Letter_Design_R.png", // Use the 'R' image as a thumbnail
+        image: "assets/images/cnc_portfolio/Letter_R.png", // Use the 'R' image as a thumbnail
         liveUrl: "metal-lettering.html", // Links to the new page
         repoUrl: ""
     }
